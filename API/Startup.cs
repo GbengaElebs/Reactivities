97d496d0fc6f6498f46e21b70970758a1a78bbbe
mac <rukyelebs@gmail.com>
2020-06-16 23:13:55 +0100

    Initial Commit

---
+++ API/Startup.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;// inject configuration
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();//dependency injection container services we need to add in our application that we want available everywhere
+            services.AddDbContext<DataContext>(opt => 
+            {
+                opt.UseSqlite(Configuration.GetConnectionString
+                ("DefaultConnection"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            ///middle ware that we add to the pipeline
+
+            //app.UseHttpsRedirection();////use selfsigned request routes request to https
+
+            app.UseRouting();////middleware to tell application to use routing
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();///controller endpoints so the api knows what to do when a request comes into the api and how to routes it
+            });
+        }
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions options): base(options)
+        {
+            ///use options from the base class
+        }
+
+        public DbSet<Value> Values { get; set;}
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value {Id=1, Name = "Value Gbenga"},
+                    new Value {Id=2, Name = "Value Joshua"},
+                    new Value {Id=3, Name = "Value Shola"}
+                );
+        }
+    }
+}
