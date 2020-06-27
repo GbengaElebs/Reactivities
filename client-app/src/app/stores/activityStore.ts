39eda0a01019c7798e8dfe6a76601cddf8b47bdf
mac <rukyelebs@gmail.com>
2020-06-27 23:12:47 +0100

    AddedStyles

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,70 +1,86 @@
-import { observable, action, computed,configure, runInAction} from "mobx";
+import { observable, action, computed, configure, runInAction } from "mobx";
 import { createContext, SyntheticEvent } from "react";
 import { IActivity } from "../Model/activity";
 import agent from "../api/agent";
 
-configure({ enforceActions: 'always'});
+configure({ enforceActions: "always" });
 
 class ActivityStore {
   @observable activityRegistry = new Map();
   @observable selectedActivity: IActivity | null | undefined = null;
   @observable loadingInitial = false;
   @observable submitting = false;
-  @observable target = '';
+  @observable target = "";
 
   @computed get activitiesByDate() {
-    return Array.from(this.activityRegistry.values()).sort(
+    return this.groupActivitiesByDate(
+      Array.from(this.activityRegistry.values())
+    );
+  }
+
+  groupActivitiesByDate(activities: IActivity[]) {
+    const sortedActivities = activities.sort(
       (a, b) => Date.parse(a.dateTime) - Date.parse(b.dateTime)
     );
+    return Object.entries(sortedActivities.reduce((activities, selectedActivity) => {
+       const date = selectedActivity.dateTime.split('T')[0];
+       activities[date] = activities[date] ? [...activities[date], selectedActivity] : [selectedActivity];
+       return activities;
+    },{} as {[key: string]: IActivity[]}));
+    ////return a new object withe a key and the value
+    ///var object= {
+     ////key[spetember2019]: [Activity1],
+     ////                     [Activity2]
+    /////}
   }
 
   @action loadActivites = async () => {
     this.loadingInitial = true;
     try {
-      const activities = await agent.Activities.list()
-      runInAction('loading activities',() => {
+      const activities = await agent.Activities.list();
+      runInAction("loading activities", () => {
         activities.forEach((activity) => {
           activity.dateTime = activity.dateTime.split(".")[0];
           this.activityRegistry.set(activity.id, activity);
         });
         this.loadingInitial = false;
-      })
+      });
     } catch (error) {
-      runInAction('loading activities error',() => {
+      runInAction("loading activities error", () => {
         this.loadingInitial = false;
-      })
+      });
       console.log(error);
     }
   };
 
   @action loadActivity = async (id: string) => {
     let activity = this.getActivity(id);
-    if(activity) {
+    if (activity) {
       this.selectedActivity = activity;
-    }else{
+    } else {
       this.loadingInitial = true;
       try {
         activity = await agent.Activities.details(id);
-        runInAction('get Activity',() => {
+        runInAction("get Activity", () => {
           this.selectedActivity = activity;
           this.loadingInitial = false;
-        })
-      } catch(error){
-        runInAction('getting Activity Error',() => {
+        });
+      } catch (error) {
+        runInAction("getting Activity Error", () => {
           this.loadingInitial = false;
-        })
+        });
         console.log(error);
       }
     }
-  }
+  };
 
-@action clearSelectedActivity =() => {
-  this.selectedActivity = null;
-}
+  @action clearSelectedActivity = () => {
+    this.selectedActivity = null;
+  };
 
-  getActivity =(id: string) => {
+  getActivity = (id: string) => {
     return this.activityRegistry.get(id);
-  }
+  };
 
   @action selectActivity = (id: string) => {
     this.selectedActivity = this.activityRegistry.get(id);
@@ -74,14 +90,14 @@ class ActivityStore {
     this.submitting = true;
     try {
       await agent.Activities.create(activity);
-      runInAction('Create activities',() => {
+      runInAction("Create activities", () => {
         this.activityRegistry.set(activity.id, activity);
-      this.submitting = false;
-      })     
-    } catch (error) {
-      runInAction('Create activities Error',() => {
         this.submitting = false;
-      })
+      });
+    } catch (error) {
+      runInAction("Create activities Error", () => {
+        this.submitting = false;
+      });
       console.log(error);
     }
   };
@@ -94,42 +110,43 @@ class ActivityStore {
     this.submitting = true;
     try {
       await agent.Activities.update(activity);
-      runInAction('edit Actitivty',() => {
+      runInAction("edit Actitivty", () => {
         this.activityRegistry.set(activity.id, activity);
         this.selectedActivity = activity;
         this.submitting = false;
-      })
+      });
     } catch (error) {
-      runInAction('edit Actitivty error',() =>{
+      runInAction("edit Actitivty error", () => {
         this.submitting = false;
-      })
+      });
       console.log(error);
     }
   };
 
-  @action deleteActivity =async (event: SyntheticEvent<HTMLButtonElement> , id : string) => {
+  @action deleteActivity = async (
+    event: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) => {
     this.submitting = true;
     this.setTarget(event);
     try {
       //const iddelete = this.activityRegistry.get(id);
       await agent.Activities.delete(id);
-      runInAction('delete Actitivity',() => {
+      runInAction("delete Actitivity", () => {
         this.activityRegistry.delete(id);
         this.submitting = false;
-      })
+      });
     } catch (error) {
-      runInAction('delete Actitivity error',() => {
-        this.target='';
+      runInAction("delete Actitivity error", () => {
+        this.target = "";
         this.submitting = false;
-      })
+      });
       console.log(error);
     }
-  }
-  @action setTarget= (event: SyntheticEvent<HTMLButtonElement> ) => {
-    this.target= event.currentTarget.name;
-  }
-
-
+  };
+  @action setTarget = (event: SyntheticEvent<HTMLButtonElement>) => {
+    this.target = event.currentTarget.name;
+  };
 }
 
 export default createContext(new ActivityStore());
--- client-app/src/features/activites/form/ActivityForm.tsx
+++ client-app/src/features/activites/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FormEvent, useContext, useEffect } from "react";
-import { Segment, Form, Button } from "semantic-ui-react";
+import { Segment, Form, Button, Grid } from "semantic-ui-react";
 import { IActivity } from "../../../app/Model/activity";
 import { v4 as uuid } from "uuid";
 import ActivityStore from "../../../app/stores/activityStore";
@@ -63,7 +63,9 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
   };
   ////spread the activity object to access its values and then append the name{object} in the activity and then replace the stuff the person is typing
   return (
-    <Segment clearing>
+    <Grid>
+      <Grid.Column width={10}>
+      <Segment clearing>
       <Form onSubmit={handleSubmit}>
         <Form.Input
           onChange={handleInputChange}
@@ -118,6 +120,9 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
         />
       </Form>
     </Segment>
+      </Grid.Column>
+    </Grid>
+    
   );
 };
 
